48d86f970cda39ce6a7d242fbb2cba9a73c28e61
coderalokdubey <149360909+coderalokdubey@users.noreply.github.com>
2024-04-16 15:57:59 +0530

    Add files via upload
    first file setup on react with vite

---
+++ Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Login</h1>
+        <label htmlFor="email">Email</label>
+        <input type="text" placeholder='Enter Email' />
+        
+      </div>
+    </div>
+  )
+}
+
+export default Login
